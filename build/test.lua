7ee180c93088315e26ff28eecb96216b2aa931e6
Will Hopkins <willothyh@gmail.com>
2023-10-17 16:03:39 -0700

    ci: test compat with older nvim versions

--- build/test.lua
+++ build/test.lua
@@ -1,25 +1,34 @@
-local cwd = vim.uv.cwd()
+coroutine.resume(coroutine.create(function()
+  local uv = vim.uv or vim.loop
 
-if
-  not vim.uv.fs_stat(cwd .. "/build/mini.test")
-  or not vim.uv.fs_stat(cwd .. "/build/mini.test/lua/")
-then
-  print("cloning mini.test")
-  vim.system({ "git", "submodule", "update", "--remote" }, {}):wait()
-  print("done")
-end
+  local cwd = uv.cwd()
 
-vim.opt.rtp:prepend(cwd)
-vim.opt.rtp:prepend(string.format("%s/build/mini.test", cwd))
+  if
+    not uv.fs_stat(cwd .. "/build/mini.test")
+    or not uv.fs_stat(cwd .. "/build/mini.test/lua/")
+  then
+    print("cloning mini.test")
+    local co = coroutine.running()
+    coroutine.yield(uv.spawn("git", {
+      args = { "submodule", "update", "--remote" },
+    }, function(...)
+      coroutine.resume(co, ...)
+    end))
+    print("done")
+  end
 
-local Test = require("mini.test")
+  vim.opt.rtp:prepend(cwd)
+  vim.opt.rtp:prepend(string.format("%s/build/mini.test", cwd))
 
-Test.setup({
-  execute = {
-    reporter = Test.gen_reporter.stdout({
-      group_depth = 99,
-      quit_on_finish = true,
-    }),
-    stop_on_error = true,
-  },
-})
+  local Test = require("mini.test")
+
+  Test.setup({
+    execute = {
+      reporter = Test.gen_reporter.stdout({
+        group_depth = 99,
+        quit_on_finish = true,
+      }),
+      stop_on_error = true,
+    },
+  })
+end))
